616fbd4f7ac170a57ed42f1c0bc6c97b3b70c76f
Deep Kumar <deepkr2210@gmail.com>
2026-08-17 00:01:13 +0530

    Time: 1906 ms (5.05%) | Memory: 55.9 MB (18.31%) - LeetSync

---
+++ 169-majority-element/majority-element.java
@@ -0,0 +1,19 @@
+// import java.util.HashMap;
+
+class Solution {
+    public int majorityElement(int[] arr) {
+         
+        for(int i=0; i<arr.length; i++){
+            int count =0;
+            for(int j=0; j<arr.length; j++){
+                if(arr[i]==arr[j]){
+                    count++;
+                }
+            }
+            if(count > arr.length/2){
+                return arr[i];
+            }
+        }
+        return -1;
+    }
+}
