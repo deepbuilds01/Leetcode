9ffeaccace0491cca8359694f947ee1f9936a140
Deep Kumar <deepkr2210@gmail.com>
2026-09-10 11:45:12 +0530

    Time: 0 ms (100.00%) | Memory: 44.4 MB (25.46%) - LeetSync

--- 61-rotate-list/rotate-list.java
+++ 61-rotate-list/rotate-list.java
@@ -1,37 +1,44 @@
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
 class Solution {
     public ListNode rotateRight(ListNode head, int k) {
-        if(head == null) return null;
 
+        // 1. Edge case
+        if (head == null || head.next == null || k == 0) {
+            return head;
+        }
+
+        // 2. Find length and tail
         int count = 1;
         ListNode temp = head;
-        while(temp.next!=null){
-            count++;
+
+        while (temp.next != null) {
             temp = temp.next;
+            count++;
         }
+
+        // 3. Avoid unnecessary rotations
         k = k % count;
 
         if (k == 0) {
             return head;
         }
-    
 
+        // 4. Make the list circular
         temp.next = head;
-        for(int i = 0; i < count-k; i++){
-            temp = temp.next;
+
+        // 5. Find the new tail
+        int steps = count - k;
+        ListNode newTail = head;
+
+        for (int i = 1; i < steps; i++) {
+            newTail = newTail.next;
         }
-        ListNode prev = temp;
-        ListNode newhead = temp.next;
-        prev.next = null;
-        return newhead;
+
+        // 6. New head is after new tail
+        ListNode newHead = newTail.next;
+
+        // 7. Break the circle
+        newTail.next = null;
+
+        return newHead;
     }
 }
