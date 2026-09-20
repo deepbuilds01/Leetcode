bb4925a8f20a433f1999220dc13a30708ffa3f1f
Deep Kumar <deepkr2210@gmail.com>
2026-09-20 19:52:04 +0530

    Time: 3 ms (85.92%) | Memory: 42.9 MB (92.92%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,29 +1,29 @@
+
+
+
 class Solution {
     public boolean isValid(String s) {
-
         StringBuilder ans = new StringBuilder();
 
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
-
             if(ch == '(' || ch == '[' || ch == '{'){
                 ans.append(ch);
             }else{
-                if(ans.length()==0){
+                if(ans.length() == 0){
                     return false;
                 }
-                char last = ans.charAt(ans.length()-1);
-                if(last == '(' && ch == ')' ||
-                last == '{' && ch == '}' ||
-                last == '[' && ch == ']' ) {
-                    ans.deleteCharAt(ans.length()-1);
-                }else{
+                char ansch = ans.charAt(ans.length()-1);
+                if( ansch == '[' && ch == ']' ||
+                    ansch == '(' && ch == ')' ||
+                    ansch == '{' && ch == '}' ){
+                        ans.deleteCharAt(ans.length()-1);
+                }
+                else{
                     return false;
                 }
             }
         }
-         return ans.length() == 0;
-
-
+        return ans.length() == 0;
     }
 }
