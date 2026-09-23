e18dd4f0a0d3c37277b92bbd8a1b5320d58f0617
Deep Kumar <deepkr2210@gmail.com>
2026-09-24 00:43:56 +0530

    Time: 4 ms (5.98%) | Memory: 43.6 MB (8.07%) - LeetSync

--- 874-backspace-string-compare/backspace-string-compare.java
+++ 874-backspace-string-compare/backspace-string-compare.java
@@ -1,32 +1,40 @@
 class Solution {
     public boolean backspaceCompare(String s, String t) {
         StringBuilder s1 = new StringBuilder();
-
+        Stack<Character> st1 = new Stack<>();
         for(int i=0; i<s.length(); i++){
             char ch = s.charAt(i);
             if(ch == '#'){
-                if(s1.length()!=0)
-                    s1.deleteCharAt(s1.length()-1);
+                if(!st1.empty())
+                    st1.pop();
             }else{
-                s1.append(ch);
+                st1.push(ch);
             }
         }
-        // System.out.print(s1);
+
+        while(!st1.empty()){
+            s1.append(st1.pop());
+        }
+        System.out.print(s1);
+
+
 
         StringBuilder s2 = new StringBuilder();
+        Stack<Character> st2 = new Stack<>();
 
         for(int i=0; i<t.length(); i++){
             char ch = t.charAt(i);
             if(ch == '#'){
-                if(s2.length()!=0){
-                    s2.deleteCharAt(s2.length()-1);
+                if(!st2.empty()){
+                    st2.pop();
                 }
             }else{
-                s2.append(ch);
-                
+                st2.push(ch);
             }
         }
-         
+        while(!st2.empty()){
+            s2.append(st2.pop());
+        }
 
         return s1.toString().equals(s2.toString());
 
